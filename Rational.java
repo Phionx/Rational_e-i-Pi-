7c514df4fba7888a1a8ff6f80cd86167f0476240
Phionx <shanjha.usa@gmail.com>
2015-11-17 11:54:44 -0500

    first

---
+++ Rational.java
@@ -0,0 +1,28 @@
+/*
+Shantanu Jha, Vincent Tang
+e^(Pi*i)
+APCS1 pd5
+HW32-Irrationality Stops Here
+2015-11-17
+*/
+public class Rational {
+private double numerator, denominator;
+//Default is rational number 0/1
+public Rational(){
+this(0,1)
+}
+//Creates Rational object with numerator a and denominator b
+public Rational(double a, double b){
+numerator = a;
+denominator = b;
+}
+
+public static String toString(){
+return "" + numerator + "/" + denominator
+}
+
+public  
+
+
+
+} 
